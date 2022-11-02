11bdb587cc303aa7b795ef9ad0f5c82b09388224
TienLort <vantien281201@gmail.com>
2022-11-02 12:18:53 +0700

    Up logout button

--- components/layoutComponents/header/header.vue
+++ components/layoutComponents/header/header.vue
@@ -1,18 +1,39 @@
 <script setup>
-const userprofile = ref([
-  {
-    title: "My Profile",
-    desc: "Account Settings",
-  },
-  {
-    title: "My Inbox",
-    desc: "Messages & Emails",
-  },
-  {
-    title: "My Tasks",
-    desc: "To-do and Daily Tasks",
-  },
-]);
+// const userprofile = ref([
+//   {
+//     title: "Name:",
+//     desc: "",
+//   },
+//   {
+//     title: "My Inbox",
+//     desc: "Messages & Emails",
+//   },
+//   {
+//     title: "My Tasks",
+//     desc: "To-do and Daily Tasks",
+//   },
+// ]);
+const {deleteToken} = useToken();
+const {
+  onFetchResponse,
+  get,
+} = useFetchApi({
+  requireAuth: true,
+  disableHandleErrorUnauthorized: false,
+})(
+  '/users/logout',
+  {immediate: false},
+);
+
+onFetchResponse(() => {
+  deleteToken();
+  return navigateTo({name: 'login'});
+});
+
+
+const logout = () => {
+  get().json().execute();
+};
 </script>
 
 <template>
@@ -32,7 +53,7 @@ const userprofile = ref([
       </v-btn>
     </template>
 
-    <v-list class="pa-6" elevation="10" rounded="lg">
+    <!-- <v-list class="pa-6" elevation="10" rounded="lg">
       <v-list-item
         class="pa-3 mb-2"
         v-for="(item, i) in userprofile"
@@ -43,10 +64,11 @@ const userprofile = ref([
         rounded="lg"
       >
       </v-list-item>
-      <v-btn block color="secondary" variant="tonal" class="mt-4 py-4"
-        >Logout</v-btn
+      
+    </v-list> -->
+    <v-btn block color="secondary" variant="tonal" class="mt-4 py-4"  @click="logout"
+        > <v-icon left  @click="logout"> mdi-logout </v-icon> Logout</v-btn
       >
-    </v-list>
   </v-menu>
 </template>
 <style scope>
